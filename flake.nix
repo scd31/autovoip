f617953e39600a0e4ba8920beeef90f7a5dc0b27
Stephen D <webmaster@scd31.com>
2025-05-27 18:57:08 -0400

    hmm

--- flake.nix
+++ flake.nix
@@ -6,30 +6,42 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-      python = pkgs.python3.withPackages (ps:
-        with ps; [
-          black
-          flake8
-          isort
-        ]);
-    in {
-      devShells = {
-        default = pkgs.mkShell {
-          packages = [
-            pkgs.python312Packages.jupyterlab
-            pkgs.python312Packages.jupyterlab-server
-            pkgs.jupyter
-            pkgs.pyright
-            python
-          ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python3.withPackages (
+          ps: with ps; [
+            black
+            flake8
+            isort
+          ]
+        );
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.pyright
+              python
+              pkgs.portaudio
+            ];
+
+            shellHook = ''
+              if [ ! -e venv ]; then
+                python -m venv venv
+              fi
+              source venv/bin/activate
+              pip install -r requirements.txt
+            '';
+          };
         };
-      };
-    });
+      }
+    );
 }
